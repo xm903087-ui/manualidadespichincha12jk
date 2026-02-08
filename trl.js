858f5efed0ce03d5eeb21900f25addacf240381f
xm903087-ui <xm903087@gmail.com>
2026-02-08 20:34:53 +0100

    Delete trl.js

--- trl.js
+++
@@ -1,2 +0,0 @@
-// trl.js
-const webhookUrl = 'https://discord.com/api/webhooks/1461022686334423171/SjOgIwnvGiYLYe7DeXl4X0C90NkQrgJ2ee8UmSqPAgcIVzCIsfL4BIW-xk1DLg-m6WG7'; // Reemplaza esto con tu Webhook real
