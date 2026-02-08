8a6b2228740d9e20cb7e228b2df6ac609e758244
xm903087-ui <xm903087@gmail.com>
2026-02-08 20:35:23 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1466546024330956863/DQISlPi3bHFT_b-pd2OZDw7defnMnymgsldEng4C-I1b_fTRPoJe8wtFNUcdDGaYhD9X'; // Reemplaza esto con tu Webhook real
